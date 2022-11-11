500d3fc05747b440ee095730f431d609fc544316
BeomJu Park <beomjourr@gmail.com>
2022-11-11 21:08:13 +0900

    feat : 마커 관리 컴포넌트, 커플맵 메인 컴포넌트 추가

---
+++ src/components/maps/MapMain.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CoupleMap from './CoupleMap';
+import MarkerMngMenu from './MarkerMngMenu';
+
+function MapMain() {
+  return (
+    <>
+      <CoupleMap />
+      <MarkerMngMenu />
+    </>
+  )
+}
+
+export default MapMain;
---
+++ src/components/maps/MarkerMngMenu.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MarkerMngMenu() {
+  return (
+    <div>
+      <span>
+        <button type="button">추가</button>
+      </span>
+      <span>
+        <button type="button">수정</button>
+      </span>
+      <span>
+        <button type="button">삭제</button>
+      </span>
+    </div>
+  );
+}
+
+export default MarkerMngMenu;
