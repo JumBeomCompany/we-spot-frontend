13c8f9b3297ed4b13b60600f0f9b3ff2ab68c867
beomju <qjawn0318@i-screamedu.co.kr>
2022-11-11 09:47:20 +0900

    feat(#5) : 샘플데이터로 마커등록, 이벤트 추가

--- src/components/maps/CoupleMap.tsx
+++ src/components/maps/CoupleMap.tsx
@@ -4,6 +4,11 @@ import styled from 'styled-components'
 const StyledMap = styled.div`width: 80vw; height: 40vw;`
 
 export default function CoupleMap() {
+  const sampleData = [
+    { latitude: 37.48619, longitude: 126.925621 },
+    { latitude: 37.47919, longitude: 126.923621 },
+    { latitude: 37.48319, longitude: 126.926921 },
+  ]
   useEffect(() => {
     const { naver } = window
     const mapOptions = {
@@ -12,9 +17,36 @@ export default function CoupleMap() {
     }
 
     const map = new naver.maps.Map('map', mapOptions)
+    const contentElements = '<div class="infowindow-container"><b>테스트 중입니다</b></div>';
+    const markers:naver.maps.Marker | any = []
+    const infowindows:naver.maps.InfoWindow | any = []
+    const listeners:any = []
+
+    sampleData.forEach((item) => {
+      markers.push(new naver.maps.Marker({
+        // position: new naver.maps.LatLng(data.latitude, data.longitude),
+        position: new naver.maps.LatLng(item.latitude, item.longitude),
+        map: map,
+      }))
+      infowindows.push(new naver.maps.InfoWindow({
+        content: contentElements,
+      }))
+    })
+
+    markers.forEach((marker:any, index: any) => {
+      listeners.push(naver.maps.Event.addListener(marker, 'click', (e) => {
+        if (infowindows[index].getMap()) {
+          infowindows[index].close();
+        } else {
+          infowindows[index].open(map, marker);
+        }
+      }))
+    })
 
     return () => {
-      // todo destroy
+      listeners.forEach((listener: any) => {
+        naver.maps.Event.removeListener(listener)
+      })
     }
   }, [])
 
