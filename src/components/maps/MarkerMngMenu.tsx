117f0246854454c920a18f2edfa5bb6345128e08
BeomJu Park <beomjourr@gmail.com>
2022-11-12 23:18:52 +0900

    style(#16) : 마커관리 메뉴 컴포넌트 스타일링
    - materialUI, styled-component 입히기

--- src/components/maps/MarkerMngMenu.tsx
+++ src/components/maps/MarkerMngMenu.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
+import { Button, Stack, Zoom } from '@mui/material'
 import styled from 'styled-components'
+import DeleteIcon from '@mui/icons-material/Delete'
 
-const StyledMenuContainer = styled.div`position: absolute; bottom: 30px; right: 20px;`
+const StyledMenuContainer = styled(Stack)`position: absolute; width: 100%; bottom: 30px; display: flex; justify-content: center;`
+const StyledButton = styled(Button)`margin-left: 10px;`
 
 function MarkerMngMenu() {
   return (
-    <StyledMenuContainer>
-      <span>
-        <button type="button">추가</button>
-      </span>
-      <span>
-        <button type="button">수정</button>
-      </span>
-      <span>
-        <button type="button">삭제</button>
-      </span>
-    </StyledMenuContainer>
+    <Zoom in>
+      <StyledMenuContainer spacing={2} direction="row">
+        <StyledButton variant="contained">추가</StyledButton>
+        <StyledButton variant="contained">수정</StyledButton>
+        <StyledButton variant="contained" endIcon={<DeleteIcon />}>삭제</StyledButton>
+      </StyledMenuContainer>
+    </Zoom>
   );
 }
 
