d5d3b1bb0f4bcdf3300c8033853589f02068619c
BeomJu Park <beomjourr@gmail.com>
2023-01-03 20:00:16 +0900

    feat(#23) : 푸터 컴포넌트 개발

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,20 +1,26 @@
+import { Box, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import React from 'react';
-import testImg from '../../../public/imgaes/test.jpg'
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function Footer() {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <div className="bottom_menu">
-      <ul>
-        <li>
-          <img src={testImg} alt="" />
-        </li>
-        <li>
-          <img src={testImg} alt="" />
-        </li>
-        <li>
-          <img src={testImg} alt="" />
-        </li>
-      </ul>
-    </div>
+    <Box className="footer" sx={{ width: '100%' }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        sx={{ height: '70px' }}
+      >
+        <BottomNavigationAction label="커플맵" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="피드" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      </BottomNavigation>
+    </Box>
   );
 }
