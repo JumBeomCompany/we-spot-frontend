41f249b505a9fc2baa99b49aa5ff7d7a2df59dc7
beomjourr <qjawn0318@i-screamedu.co.kr>
2023-07-05 15:42:27 +0900

    Feat : 헤더 컴포넌트 기본 내용 채우기

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import headerLogoImg from '../../assets/images/header-img.png';
+
+const HeaderContainer = styled.header`
+  height: 70px;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const HeaderLogo = styled.img`
+  width: 60px;
+  height: 60px;
+`;
+
+const HeaderText = styled.div`
+  color: black;
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 10px;
+  text-shadow: 3px 0 0 white;
+`
 
 export default function Header() {
   return (
-    <header>
-      <p>we spot</p>
-    </header>
+    <HeaderContainer>
+      <HeaderLogo src={headerLogoImg} alt="헤더 로고"/> 
+      <HeaderText>We Spot</HeaderText>
+    </HeaderContainer>
   );
 }
